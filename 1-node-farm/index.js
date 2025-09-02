1e1680209fa1adfab08907bd6d3c6f901c43df14
Mapelujo Abdulkareem <olamideiyanda18@gmail.com>
2025-09-02 10:32:04 +0100

    using our own modules

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const replaceTemplate = require("./modules/replaceTemplate");
+
 //////////////////////////////////
 // FILE
 
@@ -51,22 +53,6 @@ const tempCard = fs.readFileSync(
   "utf-8"
 );
 
-const replaceTemlate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-};
-
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
@@ -74,7 +60,9 @@ const server = http.createServer((req, res) => {
   if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "content-type": "text/html" });
 
-    const cardHtml = dataObj.map((el) => replaceTemlate(tempCard, el)).join("");
+    const cardHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
     const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardHtml);
 
     res.end(output);
@@ -84,7 +72,7 @@ const server = http.createServer((req, res) => {
   } else if (pathname === "/product") {
     res.writeHead(200, { "content-type": "text/html" });
     const product = dataObj[query.id];
-    const output = replaceTemlate(tempProduct, product);
+    const output = replaceTemplate(tempProduct, product);
 
     res.end(output);
 
